dcf8feec0fbe7e8d30a2314873a73adf997d510a
Sahu, S <saurav_sahu@external.mckinsey.com>
2018-10-23 12:44:00 +0530

    export renderResult

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Neo4jGraphRenderer from 'Neo4jGraphRenderer';
-import { renderResult } from "./node_modules/Neo4jGraphRenderer/assets/render-result";
 
 export default {
-  renderResult,
   Neo4jGraphRenderer
 };
